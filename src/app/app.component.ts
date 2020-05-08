351fb19aedfe859be0d1a76ae29481a0db0c6504
ibrahim mahjoubi <mahjoubiibrahim6@gmail.com>
2020-05-08 23:12:36 +0100

    part 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'customerTable';
+export class AppComponent implements OnInit {
+  title = 'customTableApp';
+  loadedCustomers :any = [];
+  rate = "70.00";
+  deposit = "500.00";
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(){
+    this.onFetchCustomer()
+  }
+
+  private onFetchCustomer() {
+    this.http
+      .get('https://jsonplaceholder.typicode.com/users')
+      .subscribe(customers => {
+        this.loadedCustomers = customers;
+        console.log(customers[0].name)
+      })
+  }
 }
